dd669374d705d4551bf8599bafe4f15d62ad6f2a
Solomon Vana <117047239+Solomon-Coding@users.noreply.github.com>
2023-05-04 21:28:34 -0500

    mostly done with the landing page

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './LandingPage.css';
-import Logo from '../../assets/images/logo_2js_games.png'
+import Logo from '../../assets/images/logo_2js_games.png';
+import {Link} from "react-router-dom";
 
 function LandingPage() {
   return (
     <main>
       <div className="LandingPage">
-        <h1>Landing Page</h1>
+        <h1>Welcome!</h1>
         <img src={Logo} alt='2js games logo'></img>
+
+        <div>
+          <Link to="/login">Log In</Link>
+          <br/>
+          <Link to="/login">Create an Account</Link>
+        </div>
+        
       </div>
     </main>
   );
