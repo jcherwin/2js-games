d2df562baa7c03dfb92c4a7d722c93b74d348c5f
Solomon Vana <117047239+Solomon-Coding@users.noreply.github.com>
2023-05-04 21:45:22 -0500

    Optimised the sign up/ sign in form. Still needs some work done

--- client/src/App.js
+++ client/src/App.js
@@ -22,3 +22,18 @@ function App() {
 }
 
 export default App;
+
+
+// import './App.css';
+// import Form from "./Form"
+
+// function App() {
+// return (
+// <div className="App">
+// <Form />
+// </div>
+
+// );
+// }
+
+// export default App;
--- client/src/pages/LoginPage/LoginPage.js
+++ client/src/pages/LoginPage/LoginPage.js
@@ -6,7 +6,7 @@ function LoginPage() {
 
 // States for registration
 const [name, setName] = useState('');
-const [email, setEmail] = useState('');
+// const [email, setEmail] = useState('');
 const [password, setPassword] = useState('');
 
 // States for checking the errors
@@ -14,16 +14,16 @@ const [submitted, setSubmitted] = useState(false);
 const [error, setError] = useState(false);
 
 // Handling the name change
-const handleName = (e) => {
+const handleUsername = (e) => {
 setName(e.target.value);
 setSubmitted(false);
 };
 
-// Handling the email change
-const handleEmail = (e) => {
-setEmail(e.target.value);
-setSubmitted(false);
-};
+// // Handling the email change
+// const handleEmail = (e) => {
+// setEmail(e.target.value);
+// setSubmitted(false);
+// };
 
 // Handling the password change
 const handlePassword = (e) => {
@@ -34,7 +34,8 @@ setSubmitted(false);
 // Handling the form submission
 const handleSubmit = (e) => {
 e.preventDefault();
-if (name === '' || email === '' || password === '') {
+// if (name === '' || email === '' || password === '') {
+if (name === '' || password === '') {
 setError(true);
 } else {
 setSubmitted(true);
@@ -50,7 +51,7 @@ className="success"
 style={{
 display: submitted ? '' : 'none',
 }}>
-<h1>User {name} successfully registered!!</h1>
+<h1>{name} successfully registered!!</h1>
 </div>
 );
 };
@@ -82,13 +83,13 @@ return (
 
 <form>
 {/* Labels and inputs for form data */}
-<label className="label">Name</label>
-<input onChange={handleName} className="input"
+<label className="label">Username</label>
+<input onChange={handleUsername} className="input"
 value={name} type="text" />
 
-<label className="label">Email</label>
+{/* <label className="label">Email</label>
 <input onChange={handleEmail} className="input"
-value={email} type="email" />
+value={email} type="email" /> */}
 
 <label className="label">Password</label>
 <input onChange={handlePassword} className="input"
