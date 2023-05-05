3e5a1e10abf838f8eaeb80d29395bdf49b8ee2a2
Solomon Vana <117047239+Solomon-Coding@users.noreply.github.com>
2023-05-04 20:45:21 -0500

    started design on the landing page

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './LandingPage.css';
+import Logo from '../../assets/images/logo_2js_games.png'
 
 function LandingPage() {
   return (
-    <div className="LandingPage">
-      <h1>Landing Page</h1>
-    </div>
+    <main>
+      <div className="LandingPage">
+        <h1>Landing Page</h1>
+        <img src={Logo} alt='2js games logo'></img>
+      </div>
+    </main>
   );
 }
 
