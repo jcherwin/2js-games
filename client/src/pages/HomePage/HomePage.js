9d4bb26d1eb54c58b4ce72a580840b1396baf85e
Jullian0426 <jullian0426@yahoo.com>
2023-05-11 20:05:07 -0500

    Redirect users to home page if already logged in

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -6,30 +6,31 @@ import { Main, Container, Div, Div1 } from './HomePageElements';
 // import Navbar from '../../components/Navbar/Navbar';
 
 function HomePage() {
-    return (
-        <Main className="HomePage">
-            <HeaderComponent />
-            <Div1>
-                <h1>Games</h1>
-                <Div className='gameList'>
-                    {/* <h1>Games</h1> */}
-                    {/* Implement logic for creating/joining a game */}
-                    {games.map((game) =>
-                        <Container className='container'>
-                            <img className='image' src={game.image} alt={game.altText}></img>
-                            <div className='overlay'>
-                                <a className='text' href={game.gameLocation} >{game.name}</a>
 
-                                <a className='text2' href={game.gitHub} target="_blank" rel="noreferrer">
-                                    {/* <img className='gitImage' src={GitHub_icon} alt='GitHub Icon'/> */}
-                                </a>
-                            </div>
-                        </Container>
-                    )}
-                </Div>
-            </Div1>
-        </Main>
-    );
+  return (
+    <Main className="HomePage">
+      <HeaderComponent />
+      <Div1>
+        <h1>Games</h1>
+        <Div className='gameList'>
+          {/* <h1>Games</h1> */}
+          {/* Implement logic for creating/joining a game */}
+          {games.map((game) =>
+            <Container className='container'>
+              <img className='image' src={game.image} alt={game.altText}></img>
+              <div className='overlay'>
+                <a className='text' href={game.gameLocation} target="_blank" rel="noreferrer">{game.name}</a>
+
+                <a className='text2' href={game.gitHub} target="_blank" rel="noreferrer">
+                  {/* <img className='gitImage' src={GitHub_icon} alt='GitHub Icon'/> */}
+                </a>
+              </div>
+            </Container>
+          )}
+        </Div>
+      </Div1>
+    </Main>
+  );
 }
 
 export default HomePage;
--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './LandingPage.css';
 import Logo from '../../assets/images/logo_2js_games.png';
 import {Link} from "react-router-dom";
 import { Main, Img, H1, Div } from './LandingPageElements';
+import AuthService from '../../utils/auth';
 
 function LandingPage() {
+
+  useEffect(() => {
+    if (AuthService.loggedIn()) {
+      window.location.assign('/home');;
+    }
+  }, []);
+
   return (
     <Main>
       <div className="LandingPage">
