fd5ec9285407a73b841b0154c2801b1b494ebe20
Duke Nukem <joncherwin@gmail.com>
2023-05-13 05:10:25 -0500

    Another fix to server file for heroku

--- server/server.js
+++ server/server.js
@@ -63,7 +63,7 @@ const startServer = async () => {
         app.use(express.static(path.join(__dirname, '../client/build')));
     }
 
-    app.get('/', (req, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../client/build/index.html'));
     });    
 
