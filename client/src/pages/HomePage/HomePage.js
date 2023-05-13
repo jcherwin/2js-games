4fb7aeb9b23955f84b9e22be0e5acc170d424435
Duke Nukem <joncherwin@gmail.com>
2023-05-13 00:34:28 -0500

    Added authentication check to all pages, added 404 page

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -2,35 +2,35 @@ import React from 'react';
 import './HomePage.css';
 import games from '../../components/GameList/GameList';
 import HeaderComponent from '../../components/Header/Header';
-import { Main, Container, Div, Div1,FullContainer } from './HomePageElements';
+import { Main, Container, Div, Div1, FullContainer } from './HomePageElements';
 // import Navbar from '../../components/Navbar/Navbar';
 
 function HomePage() {
 
-  return (
-    <Main className="HomePage">
-      <HeaderComponent />
-      <Div1>
-        <h1>Games</h1>
-        <Div className='gameList'>
-          {/* <h1>Games</h1> */}
-          {/* Implement logic for creating/joining a game */}
-          {games.map((game) =>
+    return (
+        <Main className="HomePage">
+            <HeaderComponent />
+            <Div1>
+                <h1>Games</h1>
+                <Div className='gameList'>
+                    {/* <h1>Games</h1> */}
+                    {/* Implement logic for creating/joining a game */}
+                    {games.map((game) =>
 
-            <FullContainer>
-              
-              <Container className='container'>
-                <a className='text' href={game.gameLocation}>
-                  <img className='image' src={game.image} alt={game.altText}/>
-                </a>
-              </Container>
-              {game.name}
-            </FullContainer>
-          )}
-        </Div>
-      </Div1>
-    </Main>
-  );
+                        <FullContainer>
+
+                            <Container className='container'>
+                                <a className='text' href={game.gameLocation}>
+                                    <img className='image' src={game.image} alt={game.altText} />
+                                </a>
+                            </Container>
+                            {game.name}
+                        </FullContainer>
+                    )}
+                </Div>
+            </Div1>
+        </Main>
+    );
 }
 
 export default HomePage;
--- client/src/pages/LoginPage/LoginPage.js
+++ client/src/pages/LoginPage/LoginPage.js
@@ -1,127 +1,135 @@
-import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import { useMutation } from '@apollo/client';
 import { LOGIN, CREATE_USER } from '../../utils/mutations';
 import Auth from '../../utils/auth';
 
-import './LoginPage.css';
 import { Main, Input } from './LoginPageElements';
+import './LoginPage.css';
 
 function LoginPage() {
-  const { state } = useLocation();
-  const { title } = state;
-  console.log(title);
 
-  // States for registration
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
-
-  // States for checking the errors
-  const [submitted, setSubmitted] = useState(false);
-  const [error, setError] = useState(false);
-
-  const [loginUser, { error: loginError }] = useMutation(LOGIN);
-  const [signupUser, { error: signupError }] = useMutation(CREATE_USER);
-
-  // Handling the name change
-  const handleUsername = (e) => {
-    setName(e.target.value);
-    setSubmitted(false);
-  };
-
-  // Handling the password change
-  const handlePassword = (e) => {
-    setPassword(e.target.value);
-    setSubmitted(false);
-  };
-
-  // Handling the form submission
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    if (name === '' || password === '') {
-      setError(true);
-    } else {
-      setError(false);
-
-      if (title === "Login") {
-        try {
-          const { data } = await loginUser({ variables: { username: name, password } });
-          console.log('Logged in:', data.login);
-
-          const token = data.login.token;
-          Auth.login(token);
-          
-          // Handle successful login, e.g., redirect to home page or save the token
-        } catch (error) {
-          console.error('Error logging in:', error);
-          // Handle login error, e.g., show an error message
-        }
-      } else if (title === "Create an Account") {
-        try {
-          const { data } = await signupUser({ variables: { username: name, password } });
-          console.log('Signed up:', data.createUser);
-
-          const token = data.createUser.token;
-          Auth.login(token);
-          // Handle successful sign up, e.g., redirect to home page or save the token
-        } catch (error) {
-          console.error('Error signing up:', error);
-          // Handle sign up error, e.g., show an error message
-        }
-      }
+    // Determines the utility of the page based off the url path
+    const [title, setTitle] = useState('');
+    switch (window.location.pathname) {
+        case "/login":
+            if(title === ''){ setTitle('Login') } 
+            break;
+        case "/signup":
+            if(title === ''){ setTitle('Create an Account') } 
+            break;
+        default: window.location.assign('/');
     }
-  };
 
-  // Showing success message
-  const successMessage = () => {
+    // States for registration
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+
+    // States for checking the errors
+    const [submitted, setSubmitted] = useState(false);
+    const [error, setError] = useState(false);
+
+    const [loginUser, { error: loginError }] = useMutation(LOGIN);
+    const [signupUser, { error: signupError }] = useMutation(CREATE_USER);
+
+    // Handling the name change
+    const handleUsername = (e) => {
+        setName(e.target.value);
+        setSubmitted(false);
+    };
+
+    // Handling the password change
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+        setSubmitted(false);
+    };
+
+    // Handling the form submission
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (name === '' || password === '') {
+            setError(true);
+        } else {
+            setError(false);
+
+            if (title === "Login") {
+                try {
+                    const { data } = await loginUser({ variables: { username: name, password } });
+                    console.log('Logged in:', data.login);
+
+                    const token = data.login.token;
+                    Auth.login(token);
+
+                    // Handle successful login, e.g., redirect to home page or save the token
+                } catch (error) {
+                    console.error('Error logging in:', error);
+                    // Handle login error, e.g., show an error message
+                }
+            } else if (title === "Create an Account") {
+                try {
+                    const { data } = await signupUser({ variables: { username: name, password } });
+                    console.log('Signed up:', data.createUser);
+
+                    const token = data.createUser.token;
+                    Auth.login(token);
+                    // Handle successful sign up, e.g., redirect to home page or save the token
+                } catch (error) {
+                    console.error('Error signing up:', error);
+                    // Handle sign up error, e.g., show an error message
+                }
+            }
+        }
+    };
+
+    // Showing success message
+    const successMessage = () => {
+        return (
+            <div
+                className="success"
+                style={{
+                    display: submitted ? '' : 'none',
+                }}>
+                <h1>{name} successfully registered!!</h1>
+            </div>
+        );
+    };
+
+    // Showing error message if error is true
+    const errorMessage = () => {
+        return (
+            <div
+                className="error"
+                style={{
+                    display: error ? '' : 'none',
+                }}>
+                <h1>Please enter all the fields</h1>
+            </div>
+        );
+    };
+
     return (
-      <div
-        className="success"
-        style={{
-          display: submitted ? '' : 'none',
-        }}>
-        <h1>{name} successfully registered!!</h1>
-      </div>
-    );
-  };
+        <Main className="form">
+            <div>
+                <h1>{title}</h1>
+            </div>
 
-  // Showing error message if error is true
-  const errorMessage = () => {
-    return (
-      <div
-        className="error"
-        style={{
-          display: error ? '' : 'none',
-        }}>
-        <h1>Please enter all the fields</h1>
-      </div>
-    );
-  };
+            {/* Calling to the methods */}
+            <div className="messages">
+                {errorMessage()}
+                {successMessage()}
+            </div>
 
-  return (
-    <Main className="form">
-      <div>
-        <h1>{title}</h1>
-      </div>
+            <form>
+                {/* Labels and inputs for form data */}
+                <label className="label">Username</label>
+                <Input onChange={handleUsername} className="input"
+                    value={name} type="text" placeholder="username" />
 
-      {/* Calling to the methods */}
-      <div className="messages">
-        {errorMessage()}
-        {successMessage()}
-      </div>
+                <label className="label">Password</label>
+                <Input onChange={handlePassword} className="input"
+                    value={password} type="password" placeholder='password' />
 
-      <form>
-        {/* Labels and inputs for form data */}
-        <label className="label">Username</label>
-        <Input onChange={handleUsername} className="input"
-          value={name} type="text" placeholder="username" />
-
-        <label className="label">Password</label>
-        <Input onChange={handlePassword} className="input"
-          value={password} type="password" placeholder='password' />
-
-        <button onClick={handleSubmit} className="btn" type="submit">
+                <button onClick={handleSubmit} className="btn" type="submit">
                     Submit
                 </button>
             </form>
